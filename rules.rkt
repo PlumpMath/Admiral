5203def1ea39df700c471762be755a7a38584045
Stephen Olsen <steve.olsen@gmail.com>
2014-02-17 16:54:10 -0500

    o boy, this isn't working at all!

--- rules.rkt
+++ rules.rkt
@@ -28,46 +28,85 @@
 ;;     seems like integer arithmatic is needed so maybe can't use
 ;;     minikanren or maybe still can or I dunno.
 
+;; ;; User rules
+;; (define rules
+;;   (list
+   ;; (def-action (rotate-clockwise)
+   ;;   (fire-rocket bs)
+   ;;   (fire-rocket sp))
 
+   ;; (def-query (near-left-boarder)
+   ;;   (< pos-x 100))
 
-;; User rules
+   ;; (def-query (near-right-boarder)
+   ;;   (> pos-x 900))
+
+   ;; (def-query (facing-right)
+   ;;   (= rotation 270))
+
+   ;; (def-query (facing-left)
+   ;;   (= rotation 90))
+
+   ;; (def-rule avoiding-left
+   ;;   (near-left-boarder)
+   ;;   (not (facing-right))
+   ;;   =>
+   ;;   (rotate-clockwise))
+
+   ;; (def-rule avoiding-right
+   ;;   (near-right-boarder)
+   ;;   (not (facing-left))
+   ;;   =>
+   ;;   (rotate-clockwise))
+
+   ;; (def-rule
+   ;;   (not (avoiding-left))
+   ;;   (not (avoiding-right))
+   ;;   =>
+   ;;   (fire-rocket boost))))
+
+;; What I need the rules to look like, post macro. First guess?
 (define rules
-  (list
-   (def-action (rotate-clockwise)
-     (fire-rocket bs)
-     (fire-rocket sp))
-
-   (def-query (near-left-boarder)
-     (< pos-x) 100)
-
-   (def-query (near-right-boarder)
-     (> pos-x 900))
-
-   (def-query (facing-right)
-     (= rotation 270))
-
-   (def-query (facing-left)
-     (= rotation 90))
-
-   (def-rule avoiding-left
-     (near-left-boarder)
-     (not (facing-right))
-     =>
-     (rotate-clockwise))
-
-   (def-rule avoiding-right
-     (near-right-boarder)
-     (not (facing-left))
-     =>
-     (rotate-clockwise))
-
-   (def-rule
-     (not (avoiding-left))
-     (not (avoiding-right))
-     =>
-     (fire-rocket boost))))
-
-
+  ;; stuff will have to be defined from the gamestate.
+  (define pos-x 99)
+  (define rotation 90)
+  (define rotate-clockwise
+    (lambda (bp bs sp ss boost)
+      (fresh ()
+        (== bs #t)
+        (== sp #t))))
+  (run* (q)
+        (fresh (near-left-border near-right-border
+                facing-right facing-left
+                avoiding-left avoiding-right
+                ;; rotate-clockwise
+                bp bs sp ss boost
+                
+                )
+          (== near-left-border (< pos-x 100))
+          (== near-right-border (> pos-x 900))
+          (== facing-right (= rotation 270))
+          (== facing-left (= rotation 90))
+          (conde
+           [(== avoiding-left #t) ;; maybe just succeed?
+            (== near-left-border #t)
+            (== facing-right #f)
+            (rotate-clockwise bp bs sp ss boost)] ;; hmm TODO
+           [(== avoiding-right #t)
+            (== near-right-border #t)
+            (== facing-left #f)
+            (rotate-clockwise bp bs sp ss boost)] ;; see above
+           [(== avoiding-left #f)
+            (== avoiding-right #f)
+            (== boost #t)]
+           )
+          (== q `(,near-left-border ,near-right-border
+                                    ,facing-right
+                                    ,facing-left
+                                    ,avoiding-left
+                                    ,avoiding-right
+                                    ,bp ,bs ,sp ,ss ,boost)))
+  )
 ;;
 
 (define (get-rockets-by-dumb-rules current-rockets state components)
