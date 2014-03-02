2a675dd0c1bfb9b72c8cc70a6040c69dcdb709ba
Stephen Olsen <steve.olsen@gmail.com>
2014-03-02 13:24:21 -0500

    factor out graphics code

--- admiral.rkt
+++ admiral.rkt
@@ -8,6 +8,7 @@ Have fun!
 (require 2htdp/universe
          2htdp/image
          "engine.rkt"
+         "render.rkt"
          "rules.rkt")
 
 (define SCREEN-WIDTH 1000)
@@ -50,80 +51,6 @@ Have fun!
                        ;;                         (faction . "purple")
                        ;;                         (rockets . (#f #t #t #f #t))))
                        )))
-
-;;;; Graphics
-;; This is a bit messy and sort of locked to 1000 x 500
-(define background-grid
-  (let ([with-x 
-         (for/fold ([scene (empty-scene SCREEN-WIDTH SCREEN-HEIGHT "darkgray")])
-             ([x (stream-map (lambda (x) (* 10 x)) (in-range 1 100))])
-           (scene+line
-            scene
-            x 0 x SCREEN-HEIGHT "lightgray"
-            ))])
-    (for/fold ([scene with-x])
-        ([y (stream-map (lambda (x) (* 10 x)) (in-range 1 50))])
-      (scene+line
-       scene
-       0 y SCREEN-WIDTH y "lightgray"
-       ))))
-
-(define (ship faction)
-  (let ([body (rectangle 20 40 "solid" faction)]
-        [engine (rectangle 15 30 "solid" faction)])
-    (overlay/offset (overlay/offset engine 20 0 engine) 0 -15 body)))
-
-(define (add-rockets ship rockets)
-  (define rocket (triangle 15 "solid" "gold"))
-  (define blank (triangle 15 0 "black"))
-  
-  (define (add-port-rockets bp bs ship)
-    (define p (rotate 90 (if bp rocket blank)))
-    (define s (rotate -90 (if bs rocket blank)))
-    (define port-rockets (overlay/offset p 32 0 s))
-    (overlay/offset port-rockets 0 18 ship))
-  
-  (define (add-stern-rockets sp ss ship)
-    (define p (rotate 90 (if sp rocket blank)))
-    (define s (rotate -90 (if ss rocket blank)))
-    (define stern-rockets (overlay/offset p 48 0 s))
-    (overlay/offset stern-rockets 0 -17 ship))
-  
-  (define (add-boosters boost ship)
-    (define booster (rotate 180 rocket))
-    (define boosters (overlay/offset booster 20 0 booster))
-    (if boost
-        (overlay/offset boosters 0 -31 ship)
-        ship))
-  
-  (match rockets
-    [(list bp bs sp ss boost)
-     (add-boosters boost
-                   (add-stern-rockets sp ss
-                                      (add-port-rockets bp bs ship)))]))
-
-(define (get-sprite model faction)
-  (cond
-    [(equal? model 'ship) (ship faction)]))
-
-(define (draw-entity scene ent)
-  (define model (get-component ent 'model))
-  (define position (get-component ent 'position))
-  (define faction (get-component ent 'faction))
-  (define rotation (get-component ent 'rotation))
-  (define rockets (get-component ent 'rockets))
-  (define sprite (get-sprite model faction))
-  (define with-rockets (add-rockets sprite rockets))
-  (place-image (rotate rotation with-rockets)
-               (first position) (second position)
-               scene))
-
-(define (render-game state)
-  ;; Draw entities.
-  (define entities (get-entities state))
-  (for/fold ([frame background-grid])
-            ([ent (hash-values entities)])
-    (draw-entity frame ent)))
 
 ;; LOGIC STUFF
 ;; So far this just returns the rockets, later user will need to be
